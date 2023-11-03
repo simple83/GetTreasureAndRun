0a875c561bde7641849f5c2c1fa5ed9efcea5a9b
DoingNe <chg1468@naver.com>
2023-11-03 19:37:36 +0900

    Update testScript.cs

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("나만 억까해");
     }
 
     // Update is called once per frame
