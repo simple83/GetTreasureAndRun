0d37831165785b5f2a92f2f096f9b7d794b940c5
ingyughost1 <chg1468@naver.com>
2023-12-28 19:43:33 +0900

    애니메이션 수정
    - 점프할때 상승, 하강 애니메이션 매끄럽게 전환되도록 수정함
    - slide3coin2 장애물 코인 위치 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@
 public class PlayerController : MonoBehaviour
 {
     Animator animator;
+    Rigidbody2D playerRigidbody;
     public AudioClip[] jumpSoundclip;
     public AudioClip slideDownSoundclip;
     int jumpSoundindex = 0;
@@ -10,6 +11,27 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float velocity = playerRigidbody.velocity.y;
+
+        if (velocity > 0) {
+            animator.SetBool("isRising", true);
+            animator.SetBool("isFalling", false);
+        }
+        else if (velocity < 0) {
+            animator.SetBool("isRising", false);
+            animator.SetBool("isFalling", true);
+        }
+        else
+        {
+            animator.SetInteger("jumpCnt", GameManager.jumpCnt);
+            animator.SetBool("isRising", false);
+            animator.SetBool("isJump", false);
+        }
     }
 
     public void Jump()
@@ -18,11 +40,11 @@ public class PlayerController : MonoBehaviour
         {
             GameManager.isSliding = false;
             GameManager.jumpCnt++;
+            animator.SetInteger("jumpCnt", GameManager.jumpCnt);
             Debug.Log("점프 횟수 : " + GameManager.jumpCnt);
             SoundManager.instance.SFXPlay("jump", jumpSoundclip[jumpSoundindex]);
             jumpSoundindex++;
             if(jumpSoundindex >= 3 ) { jumpSoundindex = 0;}
-            Rigidbody2D playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, GameManager.jumpForce);
             //이거 속도가 아니라 힘으로 바꾸자
         }
